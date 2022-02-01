d01c37174be3bc0e0b6c90b7ce91f992f0fddf3b
Jeet Sharma <jeetsharma2112@gmail.com>
2022-02-01 12:09:27 +0530

    Added Viewer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 function Home() {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 }
 export default Home;
 
 const Container = styled.main`
-  min-height: calc(100vh-70px);
+  min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
-  overflow: hidden;
+  overflow-x: hidden;
   &:before {
     background: url("/images/home-background.png") center center / cover
       no-repeat fixed;
